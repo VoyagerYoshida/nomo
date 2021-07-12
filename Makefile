6e65ea9e38ca8028f52a9feae89f89f71a2bb487
VoyagerYoshida <yoshida@ss.cs.osakafu-u.ac.jp>
2021-07-12 13:38:29 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,16 @@
+IMAGE_NAME := voyagerwy130/pitcher-nomo:1.0
+CONTAINER_NAME := nomo
+VOLUMEDIR := /var/www/
+PWD := $(shell pwd)
+
+.PHONY: build
+build:
+	@docker build . -t $(IMAGE_NAME)
+
+.PHONY: run
+run:
+	@docker run -it --rm --name=$(CONTAINER_NAME) -v $(PWD)/N-21:$(VOLUMEDIR)/monitored -v $(PWD)/pdf:$(VOLUMEDIR)/out $(IMAGE_NAME) 
+
+.PHONY: stop
+stop:
+	@docker rm -f $(CONTAINER_NAME)
---
+++ src/bot.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "time"
+)
+
+func remove_file(filename string) error {
+    err := os.Remove(filename)
+    if err != nil {
+        if os.IsNotExist(err) {
+            return nil
+        }
+        return err
+    }
+    return nil
+}
+
+func copy_file(read_file string , write_file string) error {
+    read_data, err := os.Open(read_file)
+    if err != nil {
+        return err
+    }
+    defer read_data.Close()
+
+    write_data, err := os.Create(write_file)
+    if err != nil {
+        return err
+    }
+    defer write_data.Close()
+
+    _, err = io.Copy(write_data, read_data)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func loop(read_file string) {
+    var timeZoneJST = time.FixedZone("Asia/Tokyo", 9*60*60)
+    time.Local = timeZoneJST
+    time.LoadLocation("Asia/Tokyo")
+    const time_layout = "20060102_150405"
+
+    for {
+        _, err := os.Stat(read_file)
+        if !os.IsNotExist(err) {
+            break
+        }
+        time.Sleep(5000 * time.Millisecond)
+    }
+
+    t := time.Now()
+    format_time := t.Format(time_layout)
+    fmt.Println("[" + format_time + "]\n>>> Find PDF file.")
+    write_file := "/var/www/out/" + format_time + ".pdf"
+
+    err := copy_file(read_file, write_file)
+    if err != nil {
+        fmt.Println("!!! Cannot copy the file.")
+        return
+    }
+    fmt.Println(">>> Copy Succeed.")
+
+    err = remove_file(read_file)
+    if err != nil {
+        fmt.Println("!!! Cannot remove original file.")
+        return
+    }
+    fmt.Println(">>> Remove Succeed.")
+    time.Sleep(5000 * time.Millisecond)
+}
+
+func main() {
+    fmt.Println("Start Nomo Process.")
+    read_file := "/var/www/monitored/CBImage.pdf"
+    for {
+        loop(read_file)
+        time.Sleep(5000 * time.Millisecond)
+    }
+}
